d5582dcb89ee1fd09110e6010340864633be248d
samiulSozib <samiulcse2018@gmail.com>
2021-06-09 10:11:49 +0600

    updated commit

--- controller/projectController.js
+++ controller/projectController.js
@@ -57,3 +57,91 @@ exports.createProjectPostController=async(req,res,next)=>{
         console.log(e)
     }
 } 
+
+exports.editProjectGetController=async(req,res,next)=>{
+    let projectId=req.params.projectId
+
+    try{
+        let project=await Project.findOne({_id:projectId})
+
+        if(!project){
+            return res.json({
+                error:'There Is No Project Availavle'
+            })
+        }
+
+        return res.render('admin/pages/dashboard/project/edit-project',{
+            title:'Edit Project',
+            error:{},
+            project
+        })
+
+    }catch(e){
+        console.log(e)
+        next(e)
+    }
+}
+
+exports.editProjectPostController=async(req,res,next)=>{
+    let projectId=req.params.projectId
+    let {title,body,customIndex,category,liveShow,githubLink}=req.body
+
+    let errors=validationResult(req).formatWith(errorFormatter)
+
+    try{
+        let project= await Project.findOne({_id:projectId})
+
+        if(!project){
+            return res.json({
+                error:"There is no project available"
+            })
+        }
+
+        if(!errors.isEmpty()){
+            return res.render('admin/pages/dashboard/project/edit-project',{
+                title:'Edit Project',
+                error:errors.mapped(),
+                project
+            })
+        }
+
+        let thumbnail=project.thumbnail
+
+        if(req.file){
+            thumbnail=`/uploads/${req.file.filename}`
+        }
+        await Project.findOneAndUpdate(
+            {_id:projectId},
+            {$set:{title,body,customIndex,category,liveShow,githubLink,thumbnail}},
+            {new:true}
+        )
+
+        return res.redirect('/dashboard')
+
+    }catch(e){
+        console.log(e)
+        next(e)
+    }
+
+}
+
+exports.getDeleteProjectController=async(req,res,next)=>{
+    let projectId=req.params.projectId
+
+    try{
+        let project=await Project.findOne({_id:projectId})
+
+        if(!project){
+            return res.json({
+                error:"Something Error"
+            })
+        }
+
+        await Project.findOneAndDelete({_id:projectId})
+        return res.redirect('/dashboard')
+
+    }catch(e){
+        console.log(e)
+        next(e)
+    }
+}
--- route/projectRoute.js
+++ route/projectRoute.js
@@ -1,10 +1,18 @@
 const router=require('express').Router()
-const {createProjectGetController,createProjectPostController}=require('../controller/projectController')
+const {createProjectGetController,createProjectPostController,editProjectGetController,editProjectPostController,getDeleteProjectController}=require('../controller/projectController')
 const {isAuthenticated}=require('../middlewares/authMiddleWare')
 const upload=require('../middlewares/uploadMiddleware')
 const projectValidator=require('../validator/dashboard/project/createProjectValidator')
 
 router.get('/create-project',isAuthenticated,createProjectGetController)
 router.post('/create-project',isAuthenticated,upload.single('project-thumbnail'),projectValidator,createProjectPostController)
+
+
+router.get('/edit-project/:projectId',isAuthenticated,editProjectGetController)
+router.post('/edit-project/:projectId',isAuthenticated,upload.single('project-thumbnail'),projectValidator,editProjectPostController)
+
+
+router.get('/delete-project/:projectId',isAuthenticated,getDeleteProjectController)
+
 
 module.exports=router
