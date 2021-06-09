620d3b2c9acd47cbb2f51f83720140685f03b630
samiulSozib <samiulcse2018@gmail.com>
2021-06-09 16:47:52 +0600

    update commit

--- controller/user/hompageController.js
+++ controller/user/hompageController.js
@@ -1,4 +1,17 @@
+const Details=require('../../model/details')
+
 exports.getHomepageController=async(req,res,next)=>{
-    return res.render('user/pages/homepage')
+    try{
+
+        let _details=await Details.find()
+        let details=_details[0]
+        //console.log(details)
+
+        return res.render('user/pages/homepage',{details})
+    }catch(e){
+        console.log(e)
+        next(e)
+    }
+    
 }
 
